42f22cfe1dbf38881e6b762df9f5234b524deba7
biamaxine <2023005718@ifam.edu.br>
2025-12-12 16:54:23 -0400

    build: nest g res routes/user
    -- GENERATED UserModule

--- src/api.module.ts
+++ src/api.module.ts
@@ -4,9 +4,10 @@ import { ConfigModule } from '@nestjs/config';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 import { PrismaService } from './shared/services/prisma/prisma.service';
+import { UserModule } from './routes/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
   controllers: [ApiController],
   providers: [ApiService, PrismaService],
 })
