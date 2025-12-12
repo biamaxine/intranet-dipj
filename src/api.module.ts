c67a509326d83eb9e0a6fdbe481b4d0aba9f8b22
biamaxine <2023005718@ifam.edu.br>
2025-12-12 12:07:34 -0400

    feat: UPDATE Prisma build: npm i @nestjs/config @prisma/client @prisma/adapter-pg feat: INCLUDE ConfigModule TO ApiModule feat: CREATE PrismaService IN shared/services/prisma/

--- src/api.module.ts
+++ src/api.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { PrismaService } from './shared/services/prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [ApiController],
-  providers: [ApiService],
+  providers: [ApiService, PrismaService],
 })
 export class ApiModule {}
---
+++ src/shared/services/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(private readonly config: ConfigService) {
+    const connectionString = config.getOrThrow<string>('DATABASE_URL');
+    super({ adapter: new PrismaPg({ connectionString }) });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
