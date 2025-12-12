9ea71c6640511f8c394cb89f3f749284a7925a2b
biamaxine <2023005718@ifam.edu.br>
2025-12-12 17:14:13 -0400

    feat: UPDATE DepartmentModule
    -- DELETE routes/department/dto
    -- UPDATE DepartmentEntity IN routes/department/entities/
    -- CREATE DepartmentEntities IN ..
    -- UPDATE DepartmentService IN routes/department/
    -- UPDATE DepartmentController IN ..

--- src/routes/department/entities/department.entity.ts
+++ src/routes/department/entities/department.entity.ts
@@ -1,1 +1,43 @@
-export class Department {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Department } from 'generated/prisma/client';
+
+export class DepartmentEntity implements Department {
+  private static example?: DepartmentEntity;
+
+  @ApiProperty() id: string;
+  @ApiProperty({ required: false, type: String }) manager_id: string | null;
+
+  @ApiProperty() name: string;
+  @ApiProperty() acronym: string;
+  @ApiProperty({ required: false, type: String }) description: string | null;
+  @ApiProperty({ required: false, type: String }) email: string | null;
+  @ApiProperty({ required: false, type: String }) phone: string | null;
+  @ApiProperty() is_active: boolean;
+
+  @ApiProperty() created_at: Date;
+  @ApiProperty() updated_at: Date;
+  @ApiProperty({ required: false, type: Date }) deleted_at: Date | null;
+
+  constructor(props: Department) {
+    Object.assign(this, props);
+  }
+
+  static getExample(): DepartmentEntity {
+    if (!DepartmentEntity.example)
+      DepartmentEntity.example = new DepartmentEntity({
+        id: crypto.randomUUID(),
+        manager_id: null,
+        name: 'Análise',
+        acronym: 'DIPJ-SA',
+        description: null,
+        email: null,
+        phone: null,
+        is_active: true,
+        created_at: new Date(),
+        updated_at: new Date(),
+        deleted_at: null,
+      });
+
+    return DepartmentEntity.example;
+  }
+}
