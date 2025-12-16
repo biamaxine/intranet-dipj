69cd49d9f3adf5d2fe2e0365a00cb1f42d1a1f67
biamaxine <2023005718@ifam.edu.br>
2025-12-16 14:28:42 -0400

    build: nest g res routes/program
    -- GENERATED ProgramModule

--- src/api.module.ts
+++ src/api.module.ts
@@ -3,13 +3,20 @@ import { ConfigModule } from '@nestjs/config';
 
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
-import { PrismaService } from './shared/services/prisma/prisma.service';
-import { UserModule } from './routes/user/user.module';
-import { DepartmentModule } from './routes/department/department.module';
 import { AuthModule } from './auth/auth.module';
+import { DepartmentModule } from './routes/department/department.module';
+import { ProgramModule } from './routes/program/program.module';
+import { UserModule } from './routes/user/user.module';
+import { PrismaService } from './shared/services/prisma/prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, DepartmentModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    DepartmentModule,
+    AuthModule,
+    ProgramModule,
+  ],
   controllers: [ApiController],
   providers: [ApiService, PrismaService],
 })
